e6fe8cde26e8fbff69ff0f7bef96e936b941ec70
Park June Chul <pjc0247@naver.com>
2016-10-02 00:27:17 +0900

    initial

---
+++ ProtocolCS/ProtocolCS/MatchMaking.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtocolCS
+{
+    /// <summary>
+    /// 클라이언트가 서버로 보내는 매칭 요청
+    /// </summary>
+    public class JoinQueue
+    {
+    }
+
+    /// <summary>
+    /// 클라이언트가 서버로 보내는 매칭 취소 요청
+    /// </summary>
+    public class LeaveQueue
+    {
+
+    }
+}
---
+++ ProtocolCS/ProtocolCS/Models.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtocolCS
+{
+    public class Player
+    {
+        public string name { get; set; }
+    }
+
+    /// <summary>
+    /// 병력이 이동 가능한 단위
+    /// </summary>
+    public class Waypoint
+    {
+
+    }
+
+    public enum CastleType
+    {
+        Normal,
+        Medium,
+        Large
+    }
+    public class Castle : Waypoint
+    {
+        public CastleType type { get; set; }
+    }
+}

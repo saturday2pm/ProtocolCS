9a245da4e6848e39f37497e6d7a65b29aa41dc6b
Park June Chul <pjc0247@naver.com>
2016-10-02 00:29:09 +0900

    1

---
+++ ProtocolCS/ProtocolCS/Constants/ProtocolVersion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtocolCS.Constants
+{
+    public class ProtocolVersion
+    {
+        public static Version version
+        {
+            get
+            {
+                return new Version(1, 0, 0);
+            }
+        }
+    }
+}

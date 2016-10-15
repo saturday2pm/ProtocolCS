5885866b3162642a60351acc8e1adabb2f92e8c5
Park June Chul <pjc0247@naver.com>
2016-10-16 04:40:51 +0900

    d

---
+++ ProtocolCS/ProtocolCS/Constants/FrameRate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtocolCS.Constants
+{
+    public class FrameRate
+    {
+        public static readonly float TargetFps = 20.0f;
+
+        public static int Interval
+        {
+            get
+            {
+                return (int)((1.0 / TargetFps) * 1000);
+            }
+        }
+    }
+}
